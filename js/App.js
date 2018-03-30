5774e94cca1157aa8d87ad0a9cef4b435804fb67
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-03-30 12:52:22 +0200

    Add setupCards function

--- js/App.js
+++ js/App.js
@@ -44,5 +44,13 @@ function setupColumns(columns) {
     columns.forEach(function (column) {
   		var col = new Column(column.id, column.name);
         board.createColumn(col);
+        setupCards(col, column.cards);
     });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(cardObj);
+  	})
 }
