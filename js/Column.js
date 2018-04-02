11b0f98877968396706478914df07bd23a2b3fe4
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-04-02 10:31:24 +0200

    Change columnAddCard

--- js/Column.js
+++ js/Column.js
@@ -18,9 +18,21 @@ function Column(id, name) {
 			self.deleteColumn();
 		});
 		
-		columnAddCard.click(function(event) {
+		$columnAddCard.click(function(event) {
+			var cardName = prompt("Enter the name of the card");
 			event.preventDefault();
-			self.createCard(new Card(prompt("Wpisz nazwę karty")));
+			$.ajax({
+    			url: baseUrl + '/card',
+    			method: 'POST',
+    			data: {
+			    name: cardName,
+			    bootcamp_kanban_column_id: self.id
+			    },
+			    success: function(response) {
+			        var card = new Card(response.id, cardName);
+			        self.createCard(card);
+			    }
+			});
 		});
 			
 			// KONSTRUOWANIE ELEMENTU KOLUMNY
